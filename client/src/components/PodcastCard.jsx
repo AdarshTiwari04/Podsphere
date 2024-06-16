0d443bb9a57a53ce770e8070f4f095930b9e65ce
Adarsh Tiwari <gautamtiwary79@gmail.com>
2024-06-16 14:01:04 +0530

    Created the PodcastCard

---
+++ client/src/components/PodcastCard.jsx
@@ -0,0 +1,195 @@
+import React from "react";
+import styled from "styled-components";
+import Avatar from "@mui/material/Avatar";
+import { IconButton } from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+
+const PlayIcon = styled.div`
+  padding: 10px;
+  border-radius: 50%;
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  background: #9000ff !important;
+  color: white !important;
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  position: absolute !important;
+  top: 45%;
+  right: 10%;
+  display: none;
+  transition: all 0.4s ease-in-out;
+  box-shadow: 0 0 16px 4px #9000ff50 !important;
+`;
+
+const Card = styled.div`
+  position: relative;
+  text-decoration: none;
+  background-color: ${({ theme }) => theme.card};
+  max-width: 220px;
+  height: 280px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 16px;
+  border-radius: 6px;
+  box-shadow: 0 0 16px 0 rgba(0, 0, 0, 0.1);
+  &:hover {
+    cursor: pointer;
+    transform: translateY(-8px);
+    transition: all 0.4s ease-in-out;
+    box-shadow: 0 0 18px 0 rgba(0, 0, 0, 0.3);
+    filter: brightness(1.3);
+  }
+
+  &:hover ${PlayIcon} {
+    display: flex;
+  }
+`;
+
+const Top = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 150px;
+  position: relative;
+`;
+
+const Favourite = styled(IconButton)`
+  color: white;
+  top: 8px;
+  right: 6px;
+  padding: 6px !important;
+  border-radius: 50%;
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  background: ${({ theme }) => theme.text_secondary + 95} !important;
+  color: white !important;
+  position: absolute !important;
+  backdrop-filter: blur(4px);
+  box-shadow: 0 0 16px 6px #222423 !important;
+`;
+
+const CardImage = styled.img`
+  object-fit: cover;
+  width: 220px;
+  height: 140px;
+  border-radius: 6px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.3);
+  &:hover {
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.4);
+  }
+`;
+
+const CardInformation = styled.div`
+  display: flex;
+  align-items: flex-end;
+  font-weight: 450;
+  padding: 14px 0px 0px 0px;
+  width: 100%;
+`;
+
+const MainInfo = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 4px;
+`;
+
+const Title = styled.div`
+  overflow: hidden;
+  display: -webkit-box;
+  max-width: 100%;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const Descryption = styled.div`
+  overflow: hidden;
+  display: -webkit-box;
+  max-width: 100%;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  color: ${({ theme }) => theme.text_secondary};
+  font-size: 12px;
+`;
+
+const CreatorInfo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 8px;
+  margin-top: 6px;
+`;
+
+const Creator = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+`;
+
+const CreatorName = styled.div`
+  font-size: 12px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  color: ${({ theme }) => theme.text_secondary};
+`;
+
+const Views = styled.div`
+  font-size: 10px;
+  color: ${({ theme }) => theme.text_secondary};
+  width: max-content;
+`;
+
+const PodcastCard = () => {
+  return (
+    <Card>
+      <div>
+        <Top>
+          <Favourite>
+            <FavoriteIcon style={{ width: "16px", height: "16px" }} />
+          </Favourite>
+          <CardImage src="https://media.istockphoto.com/id/1283532997/vector/podcast-concept-thin-line-icon-abstract-icon-abstract-gradient-background-modern-sound-wave.jpg?s=612x612&w=0&k=20&c=YLg7rHeSuYqeIuGRAcvf2a7J8X8Sx-IkmqYHXIJGPYQ=" />
+        </Top>
+        <CardInformation>
+          <MainInfo>
+            <Title>The Tim Ferriss Show</Title>
+            <Descryption>
+              {" "}
+              The Tim Ferriss Show is a podcast hosted by entrepreneur,and
+              public speaker. The podcast with world-class performsfrom a
+              variety of business,sports and more.
+            </Descryption>
+            <CreatorInfo>
+              <Creator>
+                <Avatar style={{ width: "26px", height: "26px" }}>R</Avatar>
+                <CreatorName>Rishav Chandra</CreatorName>
+              </Creator>
+              <Views> 12 Views </Views>
+            </CreatorInfo>
+          </MainInfo>
+        </CardInformation>
+      </div>
+      <PlayIcon>
+        {"video" === "video" ? (
+          <PlayArrowIcon style={{ width: "28px", height: "28px" }} />
+        ) : (
+          <HeadphonesIcon style={{ width: "28px", height: "28px" }} />
+        )}
+      </PlayIcon>
+    </Card>
+  );
+};
+
+export default PodcastCard;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,88 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import PodcastCard from "../components/PodcastCard";
+
+const DashboardMain = styled.div`
+  padding: 20px 30px;
+  padding-bottom: 200px;
+  height: 100%;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  @media (max-width: 768px) {
+    padding: 6px 10px;
+  }
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.bg};
+  border-radius: 10px;
+  padding: 20px 30px;
+`;
+
+const Topic = styled.div`
+  color: ${({ theme }) => theme.text_primary};
+  font-size: 24px;
+  font-weight: 540;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  @maedia (max-width: 768px) {
+    font-size: 18px;
+  }
+`;
+
+const Span = styled.div`
+  color: ${({ theme }) => theme.primary};
+  font-size: 16px;
+  font-weight: 400;
+  cursor: pointer;
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
+`;
+
+const Podcasts = styled.div``;
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <DashboardMain>
+      <FilterContainer>
+        <Topic>
+          Most Popular
+          <Link
+            to={"/showpodcasts/mostpopular"}
+            style={{ textDecoration: "none" }}
+          >
+            <Span>Show All</Span>
+          </Link>
+        </Topic>
+        <Podcasts>
+          <PodcastCard />
+          <PodcastCard />
+          <PodcastCard />
+        </Podcasts>
+      </FilterContainer>
+
+      <FilterContainer>
+        <Topic>
+          Comedy
+          <Link to={"/showpodcasts/comedy"} style={{ textDecoration: "none" }}>
+            <Span>Show All</Span>
+          </Link>
+        </Topic>
+        <Podcasts>
+          <PodcastCard />
+          <PodcastCard />
+          <PodcastCard />
+        </Podcasts>
+      </FilterContainer>
+    </DashboardMain>
+  );
 };
 
 export default Dashboard;
